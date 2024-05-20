f8f3a5d3cbf94e1bbeb0992aeda6e357a2d25c8d
Diego Tobalina <diegotobalina97@gmail.com>
2024-05-20 16:17:52 +0200

    Change color

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 ;(function () {
-	
+
 	'use strict';
 
 	var isMobile = {
@@ -23,7 +23,7 @@
 		}
 	};
 
-	
+
 	var fullHeight = function() {
 
 		if ( !isMobile.any() ) {
@@ -44,7 +44,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -67,9 +67,9 @@
 							el.removeClass('item-animate');
 						},  k * 100, 'easeInOutExpo' );
 					});
-					
+
 				}, 50);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -80,13 +80,13 @@
 	var goToTop = function() {
 
 		$('.js-gotop').on('click', function(event){
-			
+
 			event.preventDefault();
 
 			$('html, body').animate({
 				scrollTop: $('html').offset().top
 			}, 500, 'easeInOutExpo');
-			
+
 			return false;
 		});
 
@@ -100,7 +100,7 @@
 			}
 
 		});
-	
+
 	};
 
 	var pieChart = function() {
@@ -108,7 +108,7 @@
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
-			barColor: '#FF9000',
+			barColor: '#012561',
 			trackColor:	"#f5f5f5",
 			size: 160,
 			animate: 1000
@@ -118,9 +118,9 @@
 	var skillsWayPoint = function() {
 		if ($('#fh5co-skills').length > 0 ) {
 			$('#fh5co-skills').waypoint( function( direction ) {
-										
+
 				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
+					setTimeout( pieChart , 400);
 					$(this.element).addClass('animated');
 				}
 			} , { offset: '90%' } );
@@ -134,7 +134,7 @@
 		$(".fh5co-loader").fadeOut("slow");
 	};
 
-	
+
 	$(function(){
 		contentWayPoint();
 		goToTop();
